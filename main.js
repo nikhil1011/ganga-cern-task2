06c2329c6a48d96d9559ea198d1db6d8a3ca125d
Nikhil Vandanapu <nikhilvan10@gmail.com>
2018-03-11 15:46:45 -0500

    Finish

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ define([
 	var ncells = Jupyter.notebook.ncells();
 	for(var i = 0; i<ncells; i++){
 		var currentCell = currentCells[i];
-		if((currentCell instanceof textcell.TextCell)){
-			currentCell.element[0].innerHTML = currentCell.element[0].innerHTML.replace(/heading/g,"<img src='/nbextensions/Task2/logo_swan_cloudhisto.png' style='width:25px;height:25px;display:inline'/>");
+		if((currentCell instanceof textcell.TextCell) && (currentCell.rendered)){
+			currentCell.element[0].innerHTML = currentCell.element[0].innerHTML.replace(/SWAN/g,"<img src='/nbextensions/Task2/logo_swan_cloudhisto.png' style='width:25px;height:25px;display:inline'/>");
 		}
 	}
 	//non Jupyter notebook way to achieving the same.
 	/*$(".text_cell.rendered").html(function (_, html) {
-     		return html.replace(/heading/g,"<img src='/nbextensions/Task2/logo_swan_cloudhisto.png' style='width:20px;height:20px;display:inline'/>")
+     		return html.replace(/SWAN/g,"<img src='/nbextensions/Task2/logo_swan_cloudhisto.png' style='width:20px;height:20px;display:inline'/>")
 	});*/
     }
 
