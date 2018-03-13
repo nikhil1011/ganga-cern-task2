5928ebc098ca3c34e510cde8beb0008bc40d5924
Nikhil Vandanapu <nikhilvan10@gmail.com>
2018-03-12 23:27:21 -0500

    Change SWAN words only in text

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
 // file my_extension/main.js
-
 define([
-    'base/js/namespace','jquery','notebook/js/textcell','base/js/events'
+    'base/js/namespace', 'jquery', 'notebook/js/textcell', 'base/js/events'
 ], function(
-    Jupyter,$,textcell,events
+    Jupyter, $, textcell, events
 ) {
     function load_ipython_extension() {
         var currentCells = Jupyter.notebook.get_cells();
         var ncells = Jupyter.notebook.ncells();
         var imageLocation = '/nbextensions/Task2/logo_swan_cloudhisto.png';
-        for(var i = 0; i<ncells; i++){
+        for (var i = 0; i < ncells; i++) {
             var currentCell = currentCells[i];
-            if((currentCell instanceof textcell.TextCell) && (currentCell.rendered)){
-                currentCell.element[0].innerHTML = currentCell.element[0].innerHTML.replace(/SWAN/g,"<img src='/nbextensions/Task2/logo_swan_cloudhisto.png' style='width:30px;height:30px;display:inline'/>");
+            if ((currentCell instanceof textcell.TextCell) && (currentCell.rendered)) {
+				replaceSwanWithImage(currentCell);
             }
         }
-        
+
         //non Jupyter notebook way to achieving the same.
         /*$(".text_cell.rendered").html(function (_, html) {
                  return html.replace(/SWAN/g,"<img src='/nbextensions/Task2/logo_swan_cloudhisto.png' style='width:20px;height:20px;display:inline'/>")
         });*/
-        events.on("rendered.MarkdownCell", function (event, data) {        
-            var cellContent = data.cell.element.find('div.text_cell_render')[0];
-            cellContent.innerHTML = cellContent.innerHTML.replace(/SWAN/g,"<img src='/nbextensions/Task2/logo_swan_cloudhisto.png' style='width:30px;height:30px;display:inline'/>");
-        });   
+        events.on("rendered.MarkdownCell", function (event, data) {
+				var currentCell = data.cell;
+				replaceSwanWithImage(currentCell);
+        });
     }
-    
-    
+	
+	function replaceSwanWithImage(currentCell){
+		var parsedHTML = jQuery.parseHTML(currentCell.element[0].children[1].innerHTML);
+		var innerParsedHTML = jQuery(parsedHTML[2].innerHTML)
+		var textOfThisCell = innerParsedHTML[0].childNodes[0].textContent;
+		if (textOfThisCell.includes("SWAN")) {
+			var idOfThisCell = textOfThisCell.split(' ').join('-');
+			var textHere = $("#" + idOfThisCell).html();
+			textHere = textHere.replace(/SWAN/g, "<img src='/nbextensions/Task2/logo_swan_cloudhisto.png' style='width:20px;height:20px;display:inline'/>");
+			$("#" + idOfThisCell).html(textHere);
+		}
+	}
+
     return {
         load_ipython_extension: load_ipython_extension
     };
